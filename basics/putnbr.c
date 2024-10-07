684cd3ef1f22e937e2931987942ad650cea0dac5
Antoine-Bouclier <antoinebouclier1@gmail.com>
2024-10-07 16:59:41 +0200

    Add putnbr function

---
+++ basics/putnbr.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+void    ft_putchar(char c)
+{
+    write (1, &c, 1);
+}
+
+void    ft_putnbr(int   nb)
+{
+    if (nb == -2147483648)
+    {
+        write (1, "-2147483648", 11);
+        return ;
+    }
+    if (nb < 0)
+    {
+        nb *= -1;
+        ft_putchar('-');
+    }
+    if (nb > 9)
+    {
+        ft_putnbr(nb / 10);
+        nb = nb % 10;
+    }
+    ft_putchar(nb + '0');
+}
+
+int     main(void)
+{
+    int     nb = -1250;
+
+    ft_putnbr(nb);
+}
